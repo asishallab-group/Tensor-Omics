55d9307e69f2dfdaf5b905090a6e5e7fd9374082
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2025-06-21 23:59:37 +0200

    moved chunk related things to chunks.js, also using object functions in chunks object

--- tox_flyer/app/flyer/plotData.js
+++ tox_flyer/app/flyer/plotData.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { getChunks } from "./chunks.js";
 import {
   Mesh,
   Color,
@@ -20,108 +21,21 @@ import {
  * @param {BABYLON.Scene} scene - The BabylonJS scene in which to plot the data.
  */
 function plotData(scene) {
-  // get the initial position
-  const posX = config.get("x");
-  const posY = config.get("y");
-  const posZ = config.get("z");
-
-  // Diameter of each chunk in world units.
-  let chunkDiameter = config.get("chunkDiameter");
-
-  // Range (in number of chunks) around the current chunk that should be loaded.
-  let chunkLoadRange = config.get("chunkLoadRange");
-
-  // The maximum distance (in world units) to load/unload chunks.
-  let lastChunkDist = chunkLoadRange * chunkDiameter;
-
-  // Each key in this object corresponds to a chunk's centroid in string form.
-  let [ chunks, activeChunks ] = calculateChunks(posX, posY, posZ, chunkDiameter, chunkLoadRange);
-
-  for (const chunk of activeChunks) {
-    loadChunk(scene, chunks, chunk);
-  }
+  const chunks = getChunks(scene);
 
   setupSelectionMesh(scene);
 
-  // Determine the initial chunk centroid based on the config position.
-  // This represents the "active" chunk coordinates in which data is loaded.
-  let chunkCentroid = getChunkCentroid([posX, posY, posZ], chunkDiameter);
-
   document.addEventListener("chunkReload", (evt) => {
-    [chunks, activeChunks, chunkDiameter, chunkLoadRange] = reloadChunks(scene, chunks, activeChunks);
-    lastChunkDist = chunkLoadRange * chunkDiameter;
+    chunks.recalculate();
+    chunks.load();
     scene.getMeshByName("meshSelectedPoints").TOX_update();
-    chunkCentroid = getChunkCentroid([config.get("x"), config.get("y"), config.get("z")], chunkDiameter);
   })
-
-  // Temporary array used to compute the centroid for chunks that need updating.
-  // This array is reused within the render loop for efficiency.
-  const triggeredChunkCentroid = [0, 0, 0];
-
-  /**
-   * Register a callback that fires before every render.
-   * This callback compares the current chunk centroid (from config)
-   * with the previous one and determines which neighboring chunks need to be loaded/unloaded.
-   */
-  scene.registerBeforeRender(() => {
-    // Get the current chunk centroid from the config position.
-    const currentChunkCentroid = getChunkCentroid(
-      [ scene.activeCamera.position.x, scene.activeCamera.position.y, scene.activeCamera.position.z ],
-      chunkDiameter
-    );
-
-    // Loop through each axis (x, y, z) and detect any change in the chunk centroid.
-    // If a change is detected along an axis, adjust chunks along that axis.
-    chunkCentroid.forEach((axis, i) => {
-      const currentAxis = currentChunkCentroid[i];
-      // Only proceed if the coordinate along this axis has changed.
-      if (currentAxis !== axis) {
-
-        // Determine the direction of movement on the changed axis.
-        // If currentAxis > axis then we are moving positively (direction = 1) else negatively (direction = -1).
-        const direction = currentAxis > axis ? 1 : -1;
-
-        // Loop over all possible offsets on the remaining two axes (j and k)
-        // covering the range from -lastChunkDist to lastChunkDist (in steps of chunkDiameter).
-        for (let aAxis = -lastChunkDist; aAxis <= lastChunkDist; aAxis += chunkDiameter) {
-          // Calculate the index for the first non-changing axis.
-          const j = (i + 1) % 3;
-          triggeredChunkCentroid[j] = chunkCentroid[j] + aAxis;
-          for (let bAxis = -lastChunkDist; bAxis <= lastChunkDist; bAxis += chunkDiameter) {
-            // Calculate the index for the second non-changing axis.
-            const k = (i + 2) % 3;
-            triggeredChunkCentroid[k] = chunkCentroid[k] + bAxis;
-
-            // For the changing axis, first compute the position for the chunk that should be loaded,
-            // i.e. the new chunk in range along this axis.
-            triggeredChunkCentroid[i] = currentAxis + direction * lastChunkDist;
-            // Call loadChunk with a flag "true" to enable the chunk.
-            loadChunk(scene, chunks, triggeredChunkCentroid.toString(), true);
-            activeChunks.push(triggeredChunkCentroid.toString());
-
-            // Next, compute the position for the chunk that should be disabled,
-            // i.e. the chunk that is no longer within range, so on the complementary side.
-            triggeredChunkCentroid[i] = axis - direction * lastChunkDist;
-            // Call loadChunk with the flag "false" to disable the chunk.
-            loadChunk(scene, chunks, triggeredChunkCentroid.toString(), false);
-            const chunkIndex = activeChunks.indexOf(triggeredChunkCentroid.toString());
-            if (chunkIndex !== -1) {
-              activeChunks.splice(chunkIndex, 1);
-            }
-          }
-        }
-      }
-    });
-
-    // Update the chunkCentroid to the current value for use in the next frame.
-    chunkCentroid = currentChunkCentroid;
-  });
 
   let picked = null;
   scene.onPointerObservable.add((evt) => {
     switch (evt.type) {
       case BABYLON.PointerEventTypes.POINTERTAP:
-        picked = pickFromMeshes(scene, chunks, activeChunks);
+        picked = pickFromMeshes(chunks);
         if (picked !== null) {
           const selected = scene.getMeshByName("meshSelectedPoints");
           if (!selected.TOX_unpick(picked.family, picked.geneIndex)) {
@@ -142,42 +56,6 @@ function plotData(scene) {
         break;
     }
   });
-}
-
-function create3DGridFromChunk(scene, chunk) {
-  const [chunkX, chunkY, chunkZ] = chunk.split(",").map(Number);
-  const chunkRadius = config.get("chunkDiameter") / 2;
-  const gridStep = 10;
-  const lines = [];
-
-  const left = chunkX - chunkRadius;
-  let firstLineLeft = left < 0 ? left - left % gridStep : left + 10 - left % gridStep;
-  const right = chunkX + chunkRadius;
-  const bottom = chunkY - chunkRadius;
-  let firstLineBottom = bottom < 0 ? bottom - bottom % gridStep : bottom + 10 - bottom % gridStep;
-  const top = chunkY + chunkRadius;
-  const back = chunkZ - chunkRadius;
-  let firstLineBack = back < 0 ? back - back % gridStep : back + 10 - back % gridStep;
-  const front = chunkZ + chunkRadius;
-  for (let z = firstLineBack; z <= front; z+=gridStep) {
-    for (let y = firstLineBottom; y <= top; y+=gridStep) {
-      lines.push([Vector(left, y, z), Vector(right, y, z)]);
-    }
-  }
-  for (let z = firstLineBack; z <= front; z+=gridStep) {
-    for (let x = firstLineLeft; x <= right; x+=gridStep) {
-      lines.push([Vector(x, bottom, z), Vector(x, top, z)]);
-    }
-  }
-  for (let y = firstLineBottom; y <= top; y+=gridStep) {
-    for (let x = firstLineLeft; x <= right; x+=gridStep) {
-      lines.push([Vector(x, y, back), Vector(x, y, front)]);
-    }
-  }
-
-  const grid = BABYLON.MeshBuilder.CreateLineSystem(null, { lines }, scene);
-  grid.color = Color(.5, .5, .5);
-  return grid;
 }
 
 function setupSelectionMesh(scene) {
@@ -223,7 +101,7 @@ function setupSelectionMesh(scene) {
     highlightLayer.isEnabled = true;
   }
   meshSelectedPoints.TOX_unpick = function (family, geneIndex) {
-    const instances = this.instances.filter(i => i.TOX_family === family && ((geneIndex === undefined) || (i.TOX_geneIndex === geneIndex)))
+    const instances = this.instances.filter(i => ((i.TOX_family === family) || (family === undefined)) && ((geneIndex === undefined) || (i.TOX_geneIndex === geneIndex)))
     for (const instance of instances) {
       if (this.instances.length === 1) {
         highlightLayer.isEnabled = false;
@@ -266,18 +144,18 @@ function setupSelectionMesh(scene) {
   })
 }
 
-function pickFromMeshes(scene, chunks, activeChunks) {
-  const pickRay = scene.createPickingRay(
-    scene.pointerX, scene.pointerY,
+function pickFromMeshes(chunks) {
+  const pickRay = chunks.scene.createPickingRay(
+    chunks.scene.pointerX, chunks.scene.pointerY,
     BABYLON.Matrix.Identity(),    // you can pass other transforms if you want
-    scene.activeCamera
+    chunks.scene.activeCamera
   );
 
 
   let closestDist = Infinity;
   let picked = null;
-  for (const chunk of activeChunks) {
-    const meshes = chunks[chunk][3];
+  for (const centroid of chunks.active) {
+    const meshes = chunks.chunks.get(centroid)[3];
     for (const [meshType, mesh] of meshes) {
       const sphereMatrices = mesh.thinInstanceGetWorldMatrices(); 
 
@@ -293,7 +171,7 @@ function pickFromMeshes(scene, chunks, activeChunks) {
           { center: spherePosition, radius: matrix[0] / 2 }
         );
         if (intersects) {
-          const distance = calcVectorDistance(scene.activeCamera.position, spherePosition);
+          const distance = calcVectorDistance(chunks.scene.activeCamera.position, spherePosition);
           if (distance < closestDist) {
             closestDist = distance;
             picked = {
@@ -301,7 +179,7 @@ function pickFromMeshes(scene, chunks, activeChunks) {
               diameter: matrix[0],
               index: i,
               is_outlier: meshType === "outliers",
-              chunk
+              centroid
             }
           }
         }
@@ -310,7 +188,7 @@ function pickFromMeshes(scene, chunks, activeChunks) {
   }
 
   if (picked) {
-    const genes = chunks[picked.chunk][0];
+    const genes = chunks.chunks.get(picked.centroid)[0];
     let pickedIndex = picked.index;
     for (const [family, members] of genes) {
       const indices = members[Number(picked.is_outlier)];
@@ -323,206 +201,6 @@ function pickFromMeshes(scene, chunks, activeChunks) {
     }
   }
   return picked;
-}
-
-function reloadChunks(scene, chunks, activeChunks) {
-  clearChunks(scene, chunks, activeChunks);
-  const chunkDiameter = config.get("chunkDiameter");
-  const chunkLoadRange = config.get("chunkLoadRange");
-  const [newChunks, newActiveChunks] = calculateChunks(
-    scene.activeCamera.position.x,
-    scene.activeCamera.position.y,
-    scene.activeCamera.position.z,
-    chunkDiameter,
-    chunkLoadRange
-  );
-  for (const chunk of newActiveChunks) {
-    loadChunk(scene, newChunks, chunk);
-  }
-  return [newChunks, newActiveChunks, chunkDiameter, chunkLoadRange];
-}
-
-function clearChunks(scene, chunks, activeChunks) {
-  for (const chunk of activeChunks) {
-    loadChunk(scene, chunks, chunk, false);
-  }
-}
-
-function calculateChunks(posX, posY, posZ, chunkDiameter, chunkLoadRange) {
-  // Each key in this object corresponds to a chunk's centroid in string form.
-  const chunks = {};
-
-  // Define the "sight" range: the distance threshold (in world units) used to decide whether a chunk is visible.
-  // The value includes an extra 0.5 chunk diameter margin.
-  const sight = (chunkLoadRange + 0.5) * chunkDiameter;
-
-  chunks.scale = config.get("scale");
-  chunks.tissues = [config.get("tissueX"), config.get("tissueY"), config.get("tissueZ")]
-
-  const familiesToShow = config.get("shownFamilies") ?? dataHandler.families;
-
-  // Loop through each family available in the data handler.
-  // For each family, iterate through the genes for specific tissues
-  // and create an instance of the outlier mesh for each data point.
-  for (const family of familiesToShow) {
-    const geneCount = dataHandler.getGeneCount(family);
-    for (let geneIndex = 0; geneIndex < geneCount; geneIndex++) {
-      const { coordinates, is_outlier } = dataHandler.getGeneData(family, geneIndex, chunks.tissues, ["is_outlier"]);
-      const scaled = coordinates.map((v) => v*chunks.scale);
-
-      // Determine the centroid of the chunk this position falls into.
-      // getChunkCentroid is assumed to return an array-like coordinate (e.g. [x, y, z])
-      // which is also used as a key in the `chunks` object.
-      const chunk = getChunkCentroid(scaled, chunkDiameter);
-
-      if (chunks[chunk] === undefined) {
-        chunks[chunk] = [new Map(), 0, 0, new Map()];
-      }
-
-      const genes = chunks[chunk][0];
-      if (!genes.has(family)) {
-        genes.set(family, [[], []]);
-      }
-      genes.get(family)[is_outlier ? 1 : 0].push(geneIndex);
-      chunks[chunk][1] += !is_outlier;
-      chunks[chunk][2] += is_outlier;
-    }
-  }
-
-  const activeChunks = [];
-  for (let x = posX - sight; x <= posX + sight; x+=chunkDiameter) {
-    for (let y = posY - sight; y <= posY + sight; y+=chunkDiameter) {
-      for (let z = posZ - sight; z <= posZ + sight; z+=chunkDiameter) {
-        const chunk = getChunkCentroid([x, y, z], chunkDiameter);
-        if (
-          Math.abs(chunk[0] - posX) < sight &&
-          Math.abs(chunk[1] - posY) < sight &&
-          Math.abs(chunk[2] - posZ) < sight
-        ) {
-          activeChunks.push(chunk.toString());
-        }
-      }
-    }
-  }
-
-  if (activeChunks.length !== (chunkLoadRange*2 + 1)**3) {
-    console.error(`Number of active chunks does not match, expected ${(chunkLoadRange*2 + 1)**3}, got ${activeChunks.length}`);
-  }
-
-  return [ chunks, activeChunks ]
-}
-
-function getChunkCentroid([ x, y, z ], diameter) {
-  function trim(a) {
-    return Math.floor((a + diameter / 2) / diameter) * diameter;
-  }
-  return [trim(x), trim(y), trim(z)];
-}
-
-function loadChunk(scene, chunks, chunk, state=true) {
-  const chunkData = chunks[chunk];
-  if (chunkData) {
-    if (state) {
-      const [genes, inlierCount, outlierCount, meshes] = chunkData;
-
-      for (const [key, mesh] of meshes) {
-        mesh.dispose();
-        meshes.delete(key);
-      }
-
-      meshes.set("grid", create3DGridFromChunk(scene, chunk));
-
-      // data points -- spheres
-      const sphereDimensionsBuffer = new Float32Array(16 * inlierCount); // the translation buffer for one position takes 16 entries (it is a 4x4 rotation matrix)
-      const sphereColorBuffer = new Float32Array(4 * inlierCount); // rgba
-      if (sphereColorBuffer.length > 0) { // false if all members are outliers
-        meshes.set("inliers", Mesh.Sphere(scene));
-      }
-
-      // outliers -- octahedrons
-      const octDimensionsBuffer = new Float32Array(16 * outlierCount); // the translation buffer for one position takes 16 entries (it is a 4x4 rotation matrix)
-      const octColorBuffer = new Float32Array(4 * outlierCount); // rgba
-      if (outlierCount > 0) {
-        const mesh = Mesh.Octahedron(scene);
-        mesh.enableEdgesRendering();
-        mesh.edgesWidth = config.get("defaultDiameter") * 12;
-        mesh.edgesColor = Color(0, 0, 0, 1); // Black edges
-        mesh.edgesShareWithThinInstances = true;
-        meshes.set("outliers", mesh);
-      }
-
-      let inlierIndex = 0;
-      let outlierIndex = 0;
-      for (const [family, [inliers, outliers]] of genes) {
-        const familyColor = Color.FromHexString(config.get(`${family}_Color`) ?? dataHandler.getColor(family));
-        for (const geneIndex of inliers) {
-          const diameter = config.get(`${family}_Diameter`) ?? config.get("defaultDiameter");
-          const pointData = dataHandler.getGeneData(family, geneIndex, chunks.tissues, []);
-          fillThinInstanceBuffers(
-            sphereDimensionsBuffer, inlierIndex * 16,
-            sphereColorBuffer, inlierIndex * 4,
-            diameter,
-            pointData.coordinates.map(v => v * chunks.scale),
-            familyColor
-          );
-          inlierIndex++;
-        }
-        const outlierColorHex = config.get(`${family}_OutlierColor`);
-        const outlierColor = outlierColorHex === undefined ? familyColor : Color.FromHexString(outlierColorHex);
-        for (const geneIndex of outliers) {
-          const pointData = dataHandler.getGeneData(family, geneIndex, chunks.tissues, []);
-          const diameter = config.get(`${family}_OutlierDiameter`) ?? config.get("defaultDiameter");
-          fillThinInstanceBuffers(
-            octDimensionsBuffer, outlierIndex * 16,
-            octColorBuffer, outlierIndex * 4,
-            diameter,
-            pointData.coordinates.map(v => v * chunks.scale),
-            outlierColor
-          );
-          outlierIndex++;
-        }
-      }
-      if (inlierIndex !== inlierCount || outlierIndex !== outlierCount) {
-        console.error("Misfilled buffers", inlierIndex, inlierCount, outlierIndex, outlierCount);
-      }
-
-      meshes.get("inliers")?.thinInstanceSetBuffer("matrix", sphereDimensionsBuffer, 16);
-      meshes.get("inliers")?.thinInstanceSetBuffer("color", sphereColorBuffer, 4);
-      meshes.get("outliers")?.thinInstanceSetBuffer("matrix", octDimensionsBuffer, 16);
-      meshes.get("outliers")?.thinInstanceSetBuffer("color", octColorBuffer, 4);
-    } else {
-      if (chunkData[0].size > 0) {
-        const meshes = chunkData[3];
-        for (const [key, mesh] of meshes) {
-          mesh.dispose();
-          meshes.delete(key);
-        }
-      } else {
-        delete chunks[chunk];
-      }
-    }
-  } else {
-    chunks[chunk] = [new Map(), 0, 0, new Map([ ["grid", create3DGridFromChunk(scene, chunk)] ])];
-  }
-}
-
-function fillThinInstanceBuffers(dimensionsBuffer, dIndex, colorBuffer, cIndex, diameter, [x, y, z], color) {
-  dimensionsBuffer[dIndex] = diameter; // set x scale
-  dimensionsBuffer[dIndex + 5] = diameter; // set y scale
-  dimensionsBuffer[dIndex + 10] = diameter; // set z scale
-
-  dimensionsBuffer[dIndex + 12] = x;
-  dimensionsBuffer[dIndex + 13] = y;
-  dimensionsBuffer[dIndex + 14] = z;
-
-  dimensionsBuffer[dIndex + 15] = 1;
-  // the unchanged indices affect the rotation of the sphere -> zero 
-
-  // setting color
-  colorBuffer[cIndex++] = color.r;
-  colorBuffer[cIndex++] = color.g;
-  colorBuffer[cIndex++] = color.b;
-  colorBuffer[cIndex] = color.a;
 }
 
 /***************************************************************
