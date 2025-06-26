f5a9f893395ab36985e0536a705e6383ac0d5707
Aaron <aaron.schroeder@th-bingen.de>
2025-06-26 12:12:09 +0200

    Annotation tables are now saved independently for each corpus

--- Protein_Function_Prediction/methods/create_annotation_table.R
+++ Protein_Function_Prediction/methods/create_annotation_table.R
@@ -1,125 +1,123 @@
 library(data.table)
+library(dplyr)
+library(readr)
 library(jsonlite)
+library(tibble)
 
-# 1. Efficiently read protein2ipr.dat line-by-line and extract protein_id and description
-cat("Reading protein2ipr.dat\n")
-protein2ipr_path <- "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/protein2ipr.dat"
-con <- file(protein2ipr_path, open = "r")
-batch_size <- 1e5
-protein_id_list <- list()
-description_list <- list()
+cat("Starting script...\n")
 
-repeat {
-  lines <- readLines(con, n = batch_size)
-  if (length(lines) == 0) break
-  split_lines <- strsplit(trimws(lines), " +")
-  protein_id_list[[length(protein_id_list)+1]] <- vapply(split_lines, function(x) x[1], character(1))
-  description_list[[length(description_list)+1]] <- vapply(split_lines, function(x) {
-    if (length(x) >= 5) paste(x[3:(length(x)-3)], collapse = " ")
-    else if (length(x) >= 3) x[3]
-    else NA_character_
-  }, character(1))
+# === 1. Read UniRef50 IDs ===
+cat("Reading UniRef50 IDs...\n")
+uniref50_headers <- readLines("./material/uniref50.fasta")
+uniref50_headers <- uniref50_headers[grepl("^>", uniref50_headers)]
+uniref50_ids <- sub("^>UniRef50_(\\w+)\\s.*", "\\1", uniref50_headers)
+uniref50_set <- new.env(hash = TRUE, parent = emptyenv())
+for (id in uniref50_ids) uniref50_set[[id]] <- TRUE
+rm(uniref50_headers, uniref50_ids)
+gc()
+
+# === 2. protein2ipr: protein_id + description (nur UniRef50) ===
+cat("Reading protein2ipr.dat in chunks...\n")
+con <- file("./material/protein2ipr.dat", open = "r")
+protein_ids <- character()
+descriptions <- character()
+while (length(lines <- readLines(con, n = 100000, warn = FALSE)) > 0) {
+  splits <- strsplit(trimws(lines), " +")
+  for (x in splits) {
+    if (length(x) >= 5) {
+      protein_ids <- c(protein_ids, x[[1]])
+      descriptions <- c(descriptions, paste(x[3:(length(x)-3)], collapse = " "))
+    } else if (length(x) >= 3) {
+      protein_ids <- c(protein_ids, x[[1]])
+      descriptions <- c(descriptions, x[3])
+    }
+  }
 }
 close(con)
 
-protein2ipr <- data.table(
-  protein_id = unlist(protein_id_list),
-  description = unlist(description_list)
-)[!is.na(description)]
-rm(protein_id_list, description_list, split_lines, lines)
+protein2ipr <- tibble(protein_id = protein_ids, description = descriptions) %>%
+  filter(!is.na(description)) %>%
+  distinct() %>%
+  filter(sapply(protein_id, function(x) !is.null(uniref50_set[[x]])))
+saveRDS(protein2ipr, "./material/protein2ipr.rds")
+write.csv(protein2ipr, "./material/protein2ipr.csv")
+rm(protein_ids, descriptions, splits, lines)
 gc()
 
-# 2. Read GO terms JSON (small file, no optimization needed)
-cat("Reading go-basic-filtered.json\n")
-go_basic_path <- "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/go-basic-filtered.json"
-go_basic <- fromJSON(go_basic_path, simplifyVector = TRUE)
-go_basic <- data.table(id = go_basic$id, GOA_label = go_basic$lbl)
-go_basic[, GO_term := sub("^GO:", "GO_", id)][, id := NULL]
+# === 3. protein2goa: protein_id + description + GOA_label (nur UniRef50) ===
+cat("Reading go-basic-filtered.json...\n")
+goa_basic <- fromJSON(
+  "./material/go-basic-filtered.json",
+  simplifyVector = TRUE
+) %>%
+  as_tibble() %>%
+  select(GO_term = id, GOA_label = lbl) %>%
+  mutate(GO_term = sub("^GO:", "GO_", GO_term))
+gc()
 
-# 3. Read GAF file line-by-line and convert to data.table
-cat("Reading goa_uniprot_all.gaf\n")
-gaf_path <- "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/goa_uniprot_all.gaf"
-con <- file(gaf_path, open = "r")
-gaf_lines <- list()
-repeat {
-  lines <- readLines(con, n = batch_size)
-  if (length(lines) == 0) break
-  lines <- lines[sapply(strsplit(lines, "\\t"), length) >= 5]  # Keep valid lines only
-  gaf_lines[[length(gaf_lines) + 1]] <- lines
+cat("Reading goa_uniprot_all.gaf in chunks...\n")
+gaf_path <- "./material/goa_uniprot_all.gaf"
+gaf_con <- file(gaf_path, open = "r")
+gaf_proteins <- character()
+gaf_terms <- character()
+while (length(lines <- readLines(gaf_con, n = 100000, warn = FALSE)) > 0) {
+  for (line in lines) {
+    cols <- strsplit(line, " +")[[1]]
+    if (length(cols) >= 5 && grepl("^GO:", cols[5])) {
+      gaf_proteins <- c(gaf_proteins, cols[2])
+      gaf_terms <- c(gaf_terms, cols[5])
+    }
+  }
 }
-close(con)
-all_lines <- unlist(gaf_lines)
-tmp_gaf <- tempfile(tmpdir = "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/tmp", fileext = ".gaf")
-writeLines(all_lines, tmp_gaf)
-gaf_dt <- fread(tmp_gaf, header = FALSE, fill = TRUE, sep = "\t")
-unlink(tmp_gaf)
-rm(gaf_lines, all_lines)
-gc()
-
-# Extract protein_id and GO_term (first column starting with GO:)
-GO_idx <- which(apply(gaf_dt, 2, function(col) any(grepl("^GO:", col))))[1]
-goa_uniprot <- data.table(
-  protein_id = gaf_dt[[2]],
-  GO_term = sub("^GO:", "GO_", gaf_dt[[GO_idx]])
+close(gaf_con)
+goa_uniprot <- tibble(
+  protein_id = gaf_proteins,
+  GO_term = sub("^GO:", "GO_", gaf_terms)
 )
-rm(gaf_dt)
+rm(gaf_proteins, gaf_terms, lines)
+gc()
+protein2goa <- goa_uniprot %>%
+  left_join(goa_basic, by = "GO_term") %>%
+  select(protein_id, GOA_label) %>%
+  filter(!is.na(GOA_label)) %>%
+  distinct()
+# Merge mit protein2ipr, damit description enthalten ist
+protein2goa <- protein2goa %>%
+  left_join(protein2ipr, by = "protein_id") %>%
+  select(protein_id, description, GOA_label) %>%
+  filter(!is.na(description))
+saveRDS(protein2goa, "./material/protein2goa.rds")
+# write.csv(protein2goa, "./material/protein2goa.csv")
+rm(goa_uniprot, goa_basic)
 gc()
 
-# 4. Parse uniprot_sprot.fasta headers
-cat("Reading uniprot_sprot.fasta\n")
-sprot_headers <- fread(
-  cmd = "grep '^>' /media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/uniprot_sprot.fasta",
-  sep = "\n", header = FALSE
-)[[1]]
-sprot_df <- data.table(
+# === 4. protein2hrd: protein_id + hrd (nur UniRef50) ===
+cat("Reading swissprot headers...\n")
+sprot_headers <- readLines("./material/uniprot_sprot.fasta")
+sprot_headers <- sprot_headers[grepl("^>", sprot_headers)]
+sprot_df <- tibble(
+  header = sprot_headers,
   protein_id = sub("^>sp\\|(\\w+)\\|.*", "\\1", sprot_headers),
   hrd = sub("^>sp\\|\\w+\\|[^ ]+ (.+) OS=.*$", "\\1", sprot_headers)
-)
-# 5. Parse uniprot_trembl.fasta headers
-cat("Reading uniprot_trembl.fasta\n")
-trembl_headers <- fread(
-  cmd = "grep '^>' /media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/uniprot_trembl.fasta",
-  sep = "\n", header = FALSE
-)[[1]]
-trembl_df <- data.table(
-  protein_id = sub("^>tr\\|(\\w+)\\|.*", "\\1", trembl_headers),
-  hrd = sub("^>tr\\|\\w+\\|[^ ]+ (.+) OS=.*$", "\\1", trembl_headers)
-)
-
-# Combine headers
-hrds <- rbindlist(list(sprot_df, trembl_df))
-rm(sprot_headers, trembl_headers, sprot_df, trembl_df)
+) %>% select(protein_id, hrd)
+rm(sprot_headers)
 gc()
 
-# 6. Join all annotation info
-cat("Creating annotation table\n")
-setkey(protein2ipr, protein_id)
-setkey(hrds, protein_id)
-setkey(goa_uniprot, protein_id)
-setkey(go_basic, GO_term)
+cat("Reading trembl headers...\n")
+trembl_headers <- readLines("./material/uniprot_trembl.fasta")
+trembl_headers <- trembl_headers[grepl("^>", trembl_headers)]
+trembl_df <- tibble(
+  header = trembl_headers,
+  protein_id = sub("^>tr\\|(\\w+)\\|.*", "\\1", trembl_headers),
+  hrd = sub("^>tr\\|\\w+\\|[^ ]+ (.+) OS=.*$", "\\1", trembl_headers)
+) %>% select(protein_id, hrd)
+protein2hrd <- bind_rows(sprot_df, trembl_df) %>%
+  filter(!is.na(hrd)) %>%
+  distinct() %>%
+  filter(sapply(protein_id, function(x) !is.null(uniref50_set[[x]])))
+saveRDS(protein2hrd, "./material/protein2hrd.rds")
+#write.csv(protein2hrd, "./material/protein2hrd.csv")
+rm(trembl_headers, sprot_df, trembl_df)
+gc()
 
-annotation_table <- merge(protein2ipr, hrds, all.x = TRUE)
-annotation_table <- merge(annotation_table, goa_uniprot, all.x = TRUE)
-annotation_table <- merge(annotation_table, go_basic, by = "GO_term", all.x = TRUE)
-annotation_table <- unique(annotation_table[!is.na(GOA_label) | !is.na(hrd)])
-
-# 7. Save the unfiltered annotation table
-saveRDS(annotation_table, "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/annotation_table_unfiltered.rds")
-
-# 8. Filter for UniRef50 IDs
-cat("Reading UniRef50 and filtering\n")
-uniref50_headers <- fread(
-  cmd = "grep '^>' /media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/uniref50.fasta",
-  sep = "\n", header = FALSE
-)[[1]]
-uniref50_ids <- sub("^>UniRef50_(\\w+)\\s.*", "\\1", uniref50_headers)
-uniref50_dt <- data.table(protein_id = uniref50_ids)
-setkey(uniref50_dt, protein_id)
-
-# Perform semi-join (retain only matching protein_ids)
-annotation_table <- annotation_table[uniref50_dt, nomatch = 0]
-
-# 9. Save filtered table
-saveRDS(annotation_table, "/media/BioNAS2/Tensor_Omics/Protein_Function_Prediction/material/annotation_table_uniref50.rds")
-
-cat("Done!\n")
+cat("All annotation tables created. Done.\n")
